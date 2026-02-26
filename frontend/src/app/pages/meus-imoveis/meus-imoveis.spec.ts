fe6ab057a43201a8a8fb4719019b34288c9dcaf4
Guilherme Henrique <guihbslima200@gmail.com>
2026-02-25 21:43:28 -0300

    fix(testes): corrigir specs do home e meus-imoveis

--- frontend/src/app/pages/meus-imoveis/meus-imoveis.spec.ts
+++ frontend/src/app/pages/meus-imoveis/meus-imoveis.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MeusImoveis } from './meus-imoveis';
+import { PropertyService } from '../../services/propertyService';
+import { of } from 'rxjs';
 
 describe('MeusImoveis', () => {
   let component: MeusImoveis;
   let fixture: ComponentFixture<MeusImoveis>;
 
+  const mockPropertyService = {
+    getMyProperties: () => of([]),
+    publishProperty: () => of(null)
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MeusImoveis]
+      imports: [MeusImoveis],
+      providers: [
+        { provide: PropertyService, useValue: mockPropertyService }
+      ]
     })
     .compileComponents();
 
